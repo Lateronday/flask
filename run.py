4e6bcb0ca548988f41dcf14f0b111aa33ecb3d4c
lateronday <komorebirrr@gmail.com>
2020-06-04 00:33:30 +0800

    update manager to manage app

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from api import create_app
+from flask_script import Manager
 
 
 if __name__ == "__main__":
     app = create_app()
+    manager = Manager(app)
     print(app.url_map)
-    app.run(debug=True)
+    manager.run()
